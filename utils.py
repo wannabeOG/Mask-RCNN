bbcc02cb83a3b5a3bafa7a6f51f44a6551944643
himal <saihimallu1010@gmail.com>
2018-07-25 02:40:47 +0800

    Adds a function

--- utils.py
+++ utils.py
@@ -47,6 +47,28 @@ def extract_bboxes(mask):
         boxes[i] = np.array([y1, x1, y2, x2])
     return boxes.astype(np.int32)
 
+def compose_image_meta(image_id, image_shape, window, active_class_ids):
+    """Takes attributes of an image and puts them in one 1D array. Use
+    parse_image_meta() to parse the values back.
+    
+    image_id: An int ID of the image. Useful for debugging.
+    image_shape: [height, width, channels]
+    window: (y1, x1, y2, x2) in pixels. The area of the image where the real
+            image is (excluding the padding)
+    active_class_ids: List of class_ids available in the dataset from which
+        the image came. Useful if training on images from multiple datasets
+        where not all classes are present in all datasets.
+    """
+    meta = np.array(
+        [image_id] +            # size=1
+        list(image_shape) +     # size=3
+        list(window) +          # size=4 (y1, x1, y2, x2) in image cooredinates
+        list(active_class_ids)  # size=num_classes
+    )
+    return meta
+
+
+
 
 def compute_iou(box, boxes, box_area, boxes_area):
     """Calculates IoU of the given box with the array of the given boxes.
